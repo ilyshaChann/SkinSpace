2886feeb9c4b2aafdd9f1766d0fb489ec6ac50f5
Ilya <109521499+ilyshaChann@users.noreply.github.com>
2023-08-13 00:02:18 +0300

    Add files via upload

---
+++ main_page/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+        colors: {
+            'bluishBlack': '#181A1E',
+            'blackGrey': '#1F2125',
+            'lemony': '#FFDB18',
+            'paleBlue': '#8F9092',
+            'blueBlack': '#2B2C30',
+            'granite': '#2F3135',
+            'blackOlive': '#3B3B33',
+            'slateGray': '#3E4045',
+        },
+    },
+  },
+  plugins: [],
+}
+
